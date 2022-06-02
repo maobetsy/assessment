c6a6aaed6b62cda213882736d239c5a384287f86
Betsy <maobetsy@gmail.com>
2022-06-02 20:52:53 +1000

    added dictionary functions

--- js/main.js
+++ js/main.js
@@ -18,3 +18,143 @@ anime({
   },
   delay: 250,
 });
+
+
+// Assigning DOM elements to variables
+const dictionaryFormWrapper = document.querySelector('.dictionary-form');
+
+const searchForm = document.querySelector('.search-form');
+
+const searchingText = document.querySelector('.searching-text');
+
+const results = document.querySelector('.dictionary-results');
+
+const wordText = document.querySelector('.word');
+
+const posText = document.querySelector('.word-part');
+
+const subtitleText = document.querySelector('.definition-subtitle');
+
+const definitionText = document.querySelector('.definition-text');
+
+const errorText = document.querySelector('.error');
+
+
+// Event Listeners
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  dictionaryFormWrapper.style.borderRadius = '.25rem'
+  dictionaryFormWrapper.style.paddingBottom = '3rem'
+  results.style.display = 'none';
+
+
+  wordText.style.display = 'none';
+  posText.style.display = 'none';
+  subtitleText.style.display = 'none';
+  definitionText.style.display = 'none';
+
+  errorText.textContent = '';
+  wordText.textContent = '';
+  posText.textContent = '';
+  subtitleText.textContent = '';
+  definitionText.textContent = '';
+
+  const searchInput = document.querySelector('.search-input');
+  let word = searchInput.value;
+
+  typeSearchingText(word);
+});
+
+
+// Functions
+
+function typeSearchingText(word) {
+  const searchingString = `Searching for <span class="search-word">'${word}'</span> ...`;
+
+  searchingText.style.display = 'block';
+  searchingText.style.marginTop = '1rem';
+
+  const typed = new Typed('.searching-text', {
+    /**
+     * @property {array} strings strings to be typed
+     */
+    strings: [
+      searchingString,
+    ],
+  
+    /**
+     * @property {number} typeSpeed type speed in milliseconds
+     */
+    typeSpeed: 0,
+
+    /**
+     * @property {boolean} fadeOut Fade out instead of backspace
+     * @property {string} fadeOutClass css class for fade animation
+     * @property {boolean} fadeOutDelay Fade out delay in milliseconds
+     */
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 50,
+  
+    /**
+     * @property {boolean} loop loop strings
+     * @property {number} loopCount amount of loops
+     */
+    loop: true,
+    loopCount: 1,
+
+    showCursor: false,
+    cursorChar: '|',
+    autoInsertCss: false,
+  });
+
+  getResult(word);
+}
+
+async function getResult(word) {
+  try {
+    const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+    const response = await fetch(`${URL}${word}`);
+    let data = await response.json();
+
+    if (data) {
+      const partOfSpeech = data[0].meanings[0].partOfSpeech;
+      const definition = data[0].meanings[0].definitions[0].definition;
+      displayResult(word, partOfSpeech, definition);
+    }
+
+  } catch (error) {
+    displayError(word);
+  }
+}
+
+
+function displayError(word) {
+  dictionaryFormWrapper.style.paddingBottom = '0';
+  dictionaryFormWrapper.style.borderRadius = '.25rem .25rem 0 0'
+  results.style.display = 'block';
+  
+  errorText.style.display = 'block';
+  errorText.textContent = `Oops! No definition found for ${word}.`;
+}
+
+function displayResult(word, partOfSpeech, definition) {
+  dictionaryFormWrapper.style.paddingBottom = '0';
+  dictionaryFormWrapper.style.borderRadius = '.25rem .25rem 0 0'
+  results.style.display = 'block';
+
+  wordText.style.display = 'block';
+  wordText.textContent = word;
+
+  posText.style.display = 'block';
+  posText.textContent = partOfSpeech;
+
+
+  definitionText.style.display = 'block';
+  definitionText.textContent = definition;
+
+  subtitleText.style.display = 'block';
+  subtitleText.textContent = 'Definition';
+
+}

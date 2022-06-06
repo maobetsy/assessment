7a0ea8b307819f6ea7478fb0688e5da83b312fff
Betsy <maobetsy@gmail.com>
2022-06-06 16:28:52 +1000

    updated js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,35 @@
-// anime.js
-anime({
-  targets: '.logo, .nav-item',
-  translateY: {
-    value: 50,
-    duration: 2300
-  },
-  rotate: {
-    value: 0,
-    duration: 1800,
-    easing: 'easeInOutSine'
-  },
-  scale: {
-    value: 1,
-    duration: 1600,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  delay: 250,
-});
+// Animated header section on page load
+
+window.onload = () => {
+  anime({
+    targets: '.logo, .nav-item',
+    translateY: {
+      value: 50,
+      duration: 2300
+    },
+    rotate: {
+      value: 0,
+      duration: 1800,
+      easing: 'easeInOutSine'
+    },
+    scale: {
+      value: 1,
+      duration: 1600,
+      delay: 800,
+      easing: 'easeInOutQuart'
+    },
+    delay: 250,
+  });
+};
 
 
 // Assigning DOM elements to variables
+
 const dictionaryFormWrapper = document.querySelector('.dictionary-form');
 
 const searchForm = document.querySelector('.search-form');
+
+const searchInput = document.querySelector('.search-input');
 
 const searchingText = document.querySelector('.searching-text');
 
@@ -41,14 +47,19 @@ const errorText = document.querySelector('.error');
 
 
 // Event Listeners
+
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  dictionaryFormWrapper.style.borderRadius = '.25rem'
-  dictionaryFormWrapper.style.paddingBottom = '3rem'
+  anime({
+    targets: '.dictionary-results',
+    translateY: -5,
+    direction: 'alternate',
+    loop: false,
+    easing: 'linear'
+  });
+
   results.style.display = 'none';
-
-
   wordText.style.display = 'none';
   posText.style.display = 'none';
   subtitleText.style.display = 'none';
@@ -60,79 +71,40 @@ searchForm.addEventListener('submit', (e) => {
   subtitleText.textContent = '';
   definitionText.textContent = '';
 
-  const searchInput = document.querySelector('.search-input');
   let word = searchInput.value;
-
-  typeSearchingText(word);
+  getResult(word);
 });
 
 
 // Functions
 
-function typeSearchingText(word) {
-  const searchingString = `Searching for <span class="search-word">'${word}'</span> ...`;
-
-  searchingText.style.display = 'block';
-  searchingText.style.marginTop = '1rem';
-
-  const typed = new Typed('.searching-text', {
-    /**
-     * @property {array} strings strings to be typed
-     */
-    strings: [
-      searchingString,
-    ],
-  
-    /**
-     * @property {number} typeSpeed type speed in milliseconds
-     */
-    typeSpeed: 0,
-
-    /**
-     * @property {boolean} fadeOut Fade out instead of backspace
-     * @property {string} fadeOutClass css class for fade animation
-     * @property {boolean} fadeOutDelay Fade out delay in milliseconds
-     */
-    fadeOut: true,
-    fadeOutClass: 'typed-fade-out',
-    fadeOutDelay: 50,
-  
-    /**
-     * @property {boolean} loop loop strings
-     * @property {number} loopCount amount of loops
-     */
-    loop: true,
-    loopCount: 1,
-
-    showCursor: false,
-    cursorChar: '|',
-    autoInsertCss: false,
-  });
-
-  getResult(word);
-}
-
 async function getResult(word) {
+  typeSearchingText(word);
+
   try {
     const URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
     const response = await fetch(`${URL}${word}`);
     let data = await response.json();
 
     if (data) {
+      anime({
+        targets: '.dictionary-results',
+        translateY: 10,
+        direction: 'alternate',
+        loop: false,
+        easing: 'linear'
+      });
+
       const partOfSpeech = data[0].meanings[0].partOfSpeech;
       const definition = data[0].meanings[0].definitions[0].definition;
       displayResult(word, partOfSpeech, definition);
     }
-
   } catch (error) {
     displayError(word);
   }
 }
 
-
 function displayError(word) {
-  dictionaryFormWrapper.style.paddingBottom = '0';
-  dictionaryFormWrapper.style.borderRadius = '.25rem .25rem 0 0'
   results.style.display = 'block';
   
   errorText.style.display = 'block';
@@ -140,8 +112,6 @@ function displayError(word) {
 }
 
 function displayResult(word, partOfSpeech, definition) {
-  dictionaryFormWrapper.style.paddingBottom = '0';
-  dictionaryFormWrapper.style.borderRadius = '.25rem .25rem 0 0'
   results.style.display = 'block';
 
   wordText.style.display = 'block';
@@ -150,11 +120,49 @@ function displayResult(word, partOfSpeech, definition) {
   posText.style.display = 'block';
   posText.textContent = partOfSpeech;
 
-
   definitionText.style.display = 'block';
   definitionText.textContent = definition;
 
   subtitleText.style.display = 'block';
   subtitleText.textContent = 'Definition';
+}
 
+function typeSearchingText(word) {
+  const searchingString = `Searching for <span class="search-word">'${word}'</span> ...`;
+
+  searchingText.style.display = 'block';
+  searchingText.style.marginTop = '1rem';
+
+  const typed = new Typed('.searching-text', {
+    /*
+    @property {array} strings strings to be typed
+    */
+    strings: [
+      searchingString,
+    ],
+  
+    /*
+    @property {number} typeSpeed type speed in milliseconds
+    */
+    typeSpeed: 0,
+
+    /*
+    @property {string} fadeOutClass css class for fade animation
+    @property {boolean} fadeOutDelay Fade out delay in milliseconds
+    */
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 50,
+  
+    /*
+     @property {boolean} loop loop strings
+     @property {number} loopCount amount of loops
+    */
+    loop: true,
+    loopCount: 1,
+
+    showCursor: false,
+    cursorChar: '|',
+    autoInsertCss: false,
+  });
 }
